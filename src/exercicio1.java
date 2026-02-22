41ee8ad75e8a94df164cd5143116a899fc5c8e9e
Yuri Marvila <yurimarvilapessoal@gmail.com>
2026-02-21 21:13:08 -0300

    feat: :sparkles: adiciona exercícios de validação de senha, tabuada, conversão de temperatura e jogo de adivinhação

---
+++ src/exercicio1.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class exercicio1_numint{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // codigo padrão para capitura do teclado
+
+        // Lendo uma string - pegando a informação (system.out)
+        System.out.print("Digite seu nome: ");
+        String name = scanner.nextLine(); // ler linhas inteiras (scanner.nextLine)
+        System.out.println("Prazer... " + name);
+        // ler numeros inteiros (int / scanner.nextInt)
+
+        System.out.println("Obrigado pelas informação! Mas agora me responda");
+
+        try {
+
+            System.out.print("Digite um numero inteiro: ");
+
+            if (!scanner.hasNextInt()) { // se o usuario digitar algo que não seja um numero inteiro, o programa vai
+                                         // pedir para digitar novamente
+                System.out.println("Informaçoes invalidas. Digite numero valido!");
+                scanner.next();
+
+                System.out.print("Digite um numero inteiro: ");
+
+            }
+
+            int numinteiro = scanner.nextInt(); //
+            String resultado = "Esse número é ";
+
+            if (numinteiro > 0)
+                resultado += "Positivo e ";
+
+            else if (numinteiro < 0)
+                resultado += "Negativo e ";
+
+            else
+                resultado += "Zero e ";
+
+            if (numinteiro % 2 == 0)
+                resultado += "é par";
+
+            else
+                resultado += "é impar";
+
+            if (numinteiro % 3 == 0)
+                resultado += ", é divisivel por 3";
+
+            else
+                resultado += ", não é divisivel por 3";
+
+            System.out.println(resultado);
+        } finally {
+        }
+
+        scanner.close();
+    }
+
+}
---
+++ src/exercicio4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class exercicio4_temperatura {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // codigo padrão para capitura do teclado
+
+        try {
+            System.out.print("Digite á temperatura atual em Celsius: ");
+
+            if (!scanner.hasNextDouble()) { // se o usuario digitar algo que não seja um numero inteiro, o programa vai
+                // pedir para digitar novamente
+                System.out.println("Informaçoes invalidas. Digite numero valido!");
+                scanner.next();
+
+                System.out.print("Digite á temperatura atual em Celsius: ");
+
+            }
+
+            double temperatura = scanner.nextDouble();
+
+            double tempfarenheit = (temperatura * 9.0 / 5.0) + 32.0; // fórmula de conversão de Celsius para Farenheit
+            System.out.println("Temperatura em Farenheit: " + tempfarenheit + "°F");
+
+            double tempkelvin = temperatura + 273.15; // fórmula de conversão de Celsius para Kelvin
+            System.out.println("Temperatura em Kelvin: " + tempkelvin + "K");
+
+            scanner.close();
+
+        } finally {
+            scanner.close();
+        }
+    }
+}
